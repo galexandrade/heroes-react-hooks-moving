5bdf5ab0d21c23c6fad97747b5077b9eeb1d1329
Alex Andrade <g.alex.andrade@gmail.com>
2019-04-01 20:41:29 -0600

    Moving toolbar

--- README.md
+++ README.md
@@ -10,6 +10,16 @@
 ![alt text](https://i.ibb.co/V238NRj/Screenshot-2019-04-01-20-14-35.png)
 
 * 1 - Move `Toolbar` to function component
+```javascript
+const Toolbar = props => {
+  const [openAddModal, setOpenAddModal] = useState(false);
+
+  const switchModal = () => setOpenAddModal(!openAddModal);
+  const searchHeroHandler = event => props.searchHero(event.target.value);
+
+  {...continue...}
+}
+```
 * 2 - Move `AddHeroModal` to function component
 * 3 - Move `App` to function component
 * 4 - Create a custom hook `useField` to control form fields
--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -1,55 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button, Input, Col, Row } from 'reactstrap';
 import AddModal from './addHeroModal/addModal';
 
-class Toolbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      openAddModal: false
-    };
+const Toolbar = props => {
+  const [openAddModal, setOpenAddModal] = useState(false);
 
-    this.switchModal = this.switchModal.bind(this);
-    this.searchHeroHandler = this.searchHeroHandler.bind(this);
-  }
+  const switchModal = () => setOpenAddModal(!openAddModal);
+  const searchHeroHandler = event => props.searchHero(event.target.value);
 
-  switchModal() {
-    this.setState(prevState => ({
-      openAddModal: !prevState.openAddModal
-    }));
-  }
-
-  searchHeroHandler(event) {
-    this.props.searchHero(event.target.value);
-  }
-
-  render() {
-    return (
-        <Row className="toolbar">
-          <Col xs={6} sm={8} md={10}>
-              <Input 
-                type="text"
-                placeholder="Search your heroe here..."
-                bsSize="lg"
-                onChange={this.searchHeroHandler} />
-          </Col>
-          <Col xs={6} sm={4} md={2}>
-              <Button
-                color="success" 
-                size="lg" 
-                block
-                onClick={this.switchModal}>
-                Add hero
-              </Button>
-          </Col>
-          {this.state.openAddModal 
-            ? <AddModal 
-                modalClosed={this.switchModal} 
-                addHero={this.props.addHero}  /> 
-            : null}
-        </Row>
-    );
-  }
+  return (
+      <Row className="toolbar">
+        <Col xs={6} sm={8} md={10}>
+            <Input 
+              type="text"
+              placeholder="Search your heroe here..."
+              bsSize="lg"
+              onChange={searchHeroHandler} />
+        </Col>
+        <Col xs={6} sm={4} md={2}>
+            <Button
+              color="success" 
+              size="lg" 
+              block
+              onClick={switchModal}>
+              Add hero
+            </Button>
+        </Col>
+        {openAddModal 
+          ? <AddModal 
+              modalClosed={switchModal} 
+              addHero={props.addHero}  /> 
+          : null}
+      </Row>
+  );
 }
 
 export default Toolbar;
