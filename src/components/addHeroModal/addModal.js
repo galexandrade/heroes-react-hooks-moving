ad77d01f9763b07c333af822ebffb4fc5302fbf9
Alex Andrade <g.alex.andrade@gmail.com>
2019-04-04 18:08:16 -0600

    Try

--- src/components/addHeroModal/addModal.js
+++ src/components/addHeroModal/addModal.js
@@ -1,170 +1,138 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Row, Col, Form, FormGroup, Label, Input } from 'reactstrap';
 import HabilityField from './habilityField';
 import axios from 'axios';
+import { useField } from '../../hooks/customHooks';
 
 const DEFAULT_IMAGE = 'https://www.pedagogie.ac-nantes.fr/medias/photo/super-heroe-volando-super-heroes-pintado-por-queyla-9738241_1436103185619-jpg';
 
-class AddModal extends Component {
-  constructor(props) {
-    super(props);
+const AddModal = props => {
+  const name = useField('');
+  const image = useField('');
+  const strength = useField(0);
+  const wisdom = useField(0);
+  const speed = useField(0);
+  const magic = useField(0);
 
-    this.state = {
-      hero: {
-        name: '',
-        image: '',
-        strength: 0,
-        wisdom: 0,
-        speed: 0,
-        magic: 0,
-      },
-      formIsValid: false
-    };
+  const [formIsValid, setFormIsValid] = useState(false);
 
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.createHero = this.createHero.bind(this);
-  }
-
-  componentDidMount() {
+  /*
+  useEffect(() => {
     console.log('Going to add EventListener');
-    document.addEventListener('mousemove', this.mouseMoveHandler);
-  }
+    document.addEventListener('mousemove', mouseMoveHandler);
 
-  componentWillUnmount() {
-    console.log('Removing event listener');
-    document.removeEventListener('mousemove', this.mouseMoveHandler);
-  }
+    return () => {
+      console.log('Removing event listener');
+      document.removeEventListener('mousemove', mouseMoveHandler);
+    }
+  }, []);
+  */
 
-  mouseMoveHandler = event => {
-    console.log(event.clientX, event.clientY);
-  }
+  const mouseMoveHandler = event => console.log(event.clientX, event.clientY);
 
-  validateForm (form) {
+  const validateForm = newHero => {
     let isValid = true;
-    if(form.hero.name.trim() === ''){
+    if(newHero.name.trim() === ''){
       isValid = false;
     }
-    else if(form.hero.image.trim() === ''){
+    else if(newHero.image.trim() === ''){
       isValid = false;
     }
     return isValid;
   }
 
-  onChangeHandler(event) {
-    const hero = {
-      ...this.state.hero,
-      [event.target.id]: event.target.value
-    }
-
-    const newState = {
-      ...this.state,
-      hero: hero
-    };
-
-    this.setState({
-      ...newState,
-      formIsValid: this.validateForm(newState)
-    });
-  }
-
-  createHero() {
-    const hero = {
-      name: this.state.hero.name,
-      image: this.state.hero.image,
+  const createHero = () => {
+    const newHero = {
+      name: name.value,
+      image: image.value,
       habilities: {
-        magic: +this.state.hero.magic,
-        speed: +this.state.hero.speed,
-        strength: +this.state.hero.strength,
-        wisdom: +this.state.hero.wisdom,
+        magic: +magic.value,
+        speed: +speed.value,
+        strength: +strength.value,
+        wisdom: +wisdom.value,
       }
     }
 
-    axios.post('https://heroes-49297.firebaseio.com/heroes.json', hero)
+    axios.post('https://heroes-49297.firebaseio.com/heroes.json', newHero)
       .then(res => {
-        this.props.modalClosed();
-        this.props.addHero(hero)
+        props.modalClosed();
+        props.addHero(newHero)
       })
       .catch(err => console.log(err));
   }
 
 
-  render() {
-    const image = this.state.hero.image !== '' ? this.state.hero.image : DEFAULT_IMAGE;
+  const imageToLoad = image.value !== '' ? image.value : DEFAULT_IMAGE;
     
-    return (
-      <Modal isOpen={true} toggle={this.props.modalClosed} backdrop={true}>
-        <ModalHeader toggle={this.props.modalClosed}>Let's create a hero</ModalHeader>
-        <ModalBody>
-          <Row>
-            <Col xs={12}>
-              <Form>
-                <FormGroup>
-                  <Label for="name">Name</Label>
-                  <Input 
-                    type="text" 
-                    id="name" 
-                    placeholder="Type your hero's name here" 
-                    onChange={this.onChangeHandler} 
-                    value={this.state.hero.name} />
-                </FormGroup>
-              </Form>
-            </Col>
-            <Col xs={12}>
-              <img 
-                src={image} 
-                alt="New hero" 
-                className="new-hero-image" />
-            </Col>
-            <Col xs={12}>
-              <Form>
-                <FormGroup>
-                  <Input 
-                    type="text" 
-                    id="image" 
-                    placeholder="Copy here the image URL" 
-                    onChange={this.onChangeHandler} 
-                    value={this.state.hero.image} />
-                </FormGroup>
-              </Form>
-            </Col>
-            <Col xs={12}>
-              <Form>
-                <HabilityField 
-                  hability="strength"
-                  value={this.state.hero.strength} 
-                  onChange={this.onChangeHandler} />
-                <HabilityField 
-                  hability="wisdom"
-                  value={this.state.hero.wisdom} 
-                  onChange={this.onChangeHandler} />
-                <HabilityField 
-                  hability="speed"
-                  value={this.state.hero.speed} 
-                  onChange={this.onChangeHandler} />
-                <HabilityField 
-                  hability="magic"
-                  value={this.state.hero.magic} 
-                  onChange={this.onChangeHandler} />
-              </Form>
-            </Col>
-          </Row>
-        </ModalBody>
-        <ModalFooter>
-          <Button 
-            color="primary" 
-            onClick={this.createHero} 
-            disabled={!this.state.formIsValid}>
-            Create
-          </Button>
-          <Button 
-            color="secondary" 
-            onClick={this.props.modalClosed}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Modal>
-    );
-  }
+  return (
+    <Modal isOpen={true} toggle={props.modalClosed} backdrop={true}>
+      <ModalHeader toggle={props.modalClosed}>Let's create a hero</ModalHeader>
+      <ModalBody>
+        <Row>
+          <Col xs={12}>
+            <Form>
+              <FormGroup>
+                <Label for="name">Name</Label>
+                <Input 
+                  type="text" 
+                  id="name" 
+                  placeholder="Type your hero's name here" 
+                  onChange={name.onChange} 
+                  value={name.value} />
+              </FormGroup>
+            </Form>
+          </Col>
+          <Col xs={12}>
+            <img 
+              src={imageToLoad} 
+              alt="New hero" 
+              className="new-hero-image" />
+          </Col>
+          <Col xs={12}>
+            <Form>
+              <FormGroup>
+                <Input 
+                  type="text" 
+                  id="image" 
+                  placeholder="Copy here the image URL" 
+                  onChange={image.onChange} 
+                  value={image.value} />
+              </FormGroup>
+            </Form>
+          </Col>
+          <Col xs={12}>
+            <Form>
+              <HabilityField 
+                hability="strength"
+                {...strength}  />
+              <HabilityField 
+                hability="wisdom"
+                {...wisdom}  />
+              <HabilityField 
+                hability="speed"
+                {...speed}  />
+              <HabilityField 
+                hability="magic"
+                {...magic} />
+            </Form>
+          </Col>
+        </Row>
+      </ModalBody>
+      <ModalFooter>
+        <Button 
+          color="primary" 
+          onClick={createHero} 
+          disabled={!formIsValid}>
+          Create
+        </Button>
+        <Button 
+          color="secondary" 
+          onClick={props.modalClosed}>
+          Cancel
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 }
 
 export default AddModal;
---
+++ src/hooks/customHooks.js
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+
+export const useField = (defaultValue, shouldValidate) => {
+    const [value, setValue] = useState(defaultValue);
+
+    const onChange = event => setValue(event.target.value);
+
+    const isValid = shouldValidate && value.trim() !== '';
+
+    return {value, onChange, isValid}
+}
